c2438e751f4e70b019684b5450c482886c5893e0
NARAYANAPPA, Sadananda (US) <sadananda.narayanappa@lmco.com>
2018-10-23 01:41:34 -0600

    Commiting by Sada

--- dproject/notebooks/modules/test.py
+++ dproject/notebooks/modules/test.py
@@ -17,3 +17,16 @@ import pkgutil
 def test1(request):
     return HttpResponse(1);
 
+#--------------------------------------------------------------------------------
+def searchTerm(request):
+    q = request.GET.get( 'q', "")
+    ret = [
+        { 'value': 'Andorra', 'data': 'AD' },
+        { 'value': 'Zimbabwe', 'data': 'ZZ' },
+        { 'value': '1AAndorra', 'data': 'AD1' },
+        { 'value': '2BAndorra', 'data': 'AD2' },
+        { 'value': '3CAndorra', 'data': 'AD3' },
+    ];
+    ret = json.dumps(ret)
+    return HttpResponse(ret);
+    
---
+++ dproject/static/js/searchterms.js
@@ -0,0 +1,4 @@
+var searchTerms = [
+    { value: 'Boeing', data: 'AD' },
+    { value: 'High Speed', data: 'ZZ' }
+];
