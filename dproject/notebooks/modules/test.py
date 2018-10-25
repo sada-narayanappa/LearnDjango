4ac9e4b585fea0feea933779f8ee90e88a1613af
NARAYANAPPA, Sadananda (US) <sadananda.narayanappa@lmco.com>
2018-10-24 23:18:08 -0600

    Commiting by Sada

--- dproject/notebooks/modules/test.py
+++ dproject/notebooks/modules/test.py
@@ -12,13 +12,61 @@ import sys
 import re
 import subprocess
 import pkgutil
+import requests #install from: http://docs.python-requests.org/en/master/
 
+def searchNewsMashape(q, proxies={ "http": None, "https": None }):
+    q = "Donald%20Trump" if not q else q#the search query
+    Your_X_Mashape_Key = "i7NAd0phvimsh5TbLJN02Xy7fCUBp1nsADljsnIQQ94H6oYsGq";
+
+    #The query parameters: (update according to your search query)
+    count = 10 #the number of items to return
+    autoCorrect = True #autoCorrectspelling
+
+    whost='https://contextualwebsearch-websearch-v1.p.mashape.com/api/Search'
+    response=requests.get(f"{whost}/WebSearchAPI?q={q}&count={count}&autocorrect={autoCorrect}",
+    headers={
+    "X-Mashape-Key": Your_X_Mashape_Key,
+    "Accept": "application/json"
+    }, proxies=proxies
+    ).json()
+
+    #Get the numer of items returned
+    totalCount = response["totalCount"];
+
+    #Get the list of most frequent searches related to the input search query
+    relatedSearch = response["relatedSearch"]
+
+    ret = []
+    #Go over each resulting item
+    for webPage in response["value"]:
+        url = webPage["url"]
+        title = webPage["title"].encode('utf-8').strip()
+        description = webPage["description"]
+        keywords = webPage["keywords"]
+        provider = webPage["provider"]["name"]
+        datePublished = webPage["datePublished"]
+
+        #Get the web page image (if exists)
+        imageUrl = webPage["image"]["url"]
+        imageHeight = webPage["image"]["height"]
+        imageWidth = webPage["image"]["width"]
+
+        thumbnail = webPage["image"]["thumbnail"]
+        thumbnailHeight = webPage["image"]["thumbnailHeight"]
+        #thumbnailWidth == webPage["image"]["thumbnailWidth"]
+
+        #An example: Output the webpage url, title and published date:
+        print(f"Url: {url}. Title: {title}. Published Date:{datePublished}.")
+        
+        ret.append({'value': title, 'data': url})
+        
+    return ret;
 
 def test1(request):
     return HttpResponse(1);
 
 #--------------------------------------------------------------------------------
-def searchTerm(request):
+def searchItems(request):
     q = request.GET.get( 'q', "")
     ret = [
         { 'value': 'Andorra', 'data': 'AD' },
@@ -29,4 +77,10 @@ def searchTerm(request):
     ];
     ret = json.dumps(ret)
     return HttpResponse(ret);
-    
+
+#--------------------------------------------------------------------------------
+def searchNews(request):
+    q = request.GET.get( 'q', "")
+    ret = searchNewsMashape(q)
+    #ret = json.dumps(ret)
+    return HttpResponse(ret);
