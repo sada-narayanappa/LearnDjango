7d49221e3da8ce6dcc8d114c205e9262bf6487d7
sada-narayanappa <sada-narayanappa>
2024-10-26 09:19:59 -0600

    Commiting

--- example_app/run_wsgi.sh
+++ example_app/run_wsgi.sh
@@ -7,7 +7,7 @@ fi
 # uncomment the following to run in https mode
 # OPTS=' --cert /tmp/cert'
 
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+export SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo ${SCRIPTPATH}
 if [ -e "${SCRIPTPATH}/application_context" ]
 then
